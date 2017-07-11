37d8ba7090d9a5f9c6b9325538351fab5518a7f3
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-11 07:06:58 -0700

    Finished JS Constructor for Examples

--- JSconstructorFunctionModel.js
+++ JSconstructorFunctionModel.js
@@ -1,11 +1,17 @@
 //Projected Functions to be written
+var allWork = [];
 
+function DisplayWork(thumbnailPath, title, description, url) {
+  this.thumbnailPath = thumbnailPath;
+  this.title = title;
+  this.description = description;
+  this.url = url;
+  allWork.push(this);
+}
 
-var newFunction = {
-  location: 'Hero Image Text',
-  WhatItDo: 'Words fade in as user scrolls webpage'
-};
-var newFunction2 = {
-  location: 'Hire Me Button',
-  WhatItDo: 'On hover button fades to another color'
-};
+var exampleOne = new DisplayWork(
+  'img/LakewoodSS.png',
+  'Lakewood Veterinary Hospital',
+  'Created for a local business',
+  'http://www.lakewood-vet-hospital.com'
+);
