4aae391e7771f41a0707c02fb69030246e3cd781
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-10 17:42:16 -0700

    JS Constructor

---
+++ JSconstructorFunctionModel.js
@@ -0,0 +1,11 @@
+//Projected Functions to be written
+
+
+var newFunction = {
+  location: 'Hero Image Text',
+  WhatItDo: 'Words fade in as user scrolls webpage'
+};
+var newFunction2 = {
+  location: 'Hire Me Button',
+  WhatItDo: 'On hover button fades to another color'
+};
