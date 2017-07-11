a78a0e570cc67b6c7005bb4c4f7c0e4a9c782dd9
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-11 11:39:05 -0700

    Ready for Lab Turn In

---
+++ app.js
@@ -0,0 +1,28 @@
+'use-strict';
+$(document).ready(function() {
+  $(window).scroll(function() {
+    $("#heroText").fadeOut('slow');
+    $("#heroText").fadeIn('slow');
+  })
+});
+
+
+//Projected Functions to be written
+var allWork = [];
+
+function DisplayWork(thumbnailPath, title, description, url) {
+  this.thumbnailPath = thumbnailPath;
+  this.title = title;
+  this.description = description;
+  this.url = url;
+  allWork.push(this);
+}
+
+var exampleOne = new DisplayWork(
+  'img/LakewoodSS.png',
+  'Lakewood Veterinary Hospital',
+  'Created for a local business',
+  'http://www.lakewood-vet-hospital.com'
+);
+
+console.log(exampleOne);
