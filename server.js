efbd0220356676277d88895c54a66bd3110e01e8
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-24 14:49:31 -0700

    fixed app.get

--- server.js
+++ server.js
@@ -8,10 +8,12 @@ const PORT = process.env.PORT || 3001;
 app.use(express.static('./'))
 
 app.post('/articles', bodyParser, function(request, response) {
-
-  console.log(request.body);
   response.send('Record posted to server!!');
 })
+
+app.get('/', (req, res) => {
+  res.sendFile('./index.html', {root: './'})
+});
 
 app.listen(PORT, function() {
   console.log(`Listening on port:" ${PORT}"`)
