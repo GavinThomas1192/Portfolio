3fa6ed80cebb37601b0dad594ebec28f4d63dd8a
GavinThomas1192 <gthomas1192@gmail.com>
2017-07-27 16:30:45 -0700

    refactored for proxy

--- modules/repo.js
+++ modules/repo.js
@@ -7,15 +7,14 @@ var app = app || {};
   repos.all = [];
   repos.requestRepos = function(callback) {
 
-    $.get('https://api.github.com/user/repos?type=owner')
-    .then(
-      data => {
-        console.log(data);
-        repos.all = data;
-      },
-      err => console.error(err)
-    )
-    .then(callback)
+    $.get('github/user/repos')
+      .then(
+        data => {
+          repos.all = data, err => console.error(err)
+            .then
+          callback();
+        }
+      )
   };
 
   repos.with = attr => repos.all.filter(repo => repo[attr]);
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use-strict';
 let express = require('express');
 let app = express();
-
+const requestProxy = require('express-request-proxy');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3001;
 
@@ -10,6 +10,17 @@ app.use(express.static('./'))
 app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
+
+function proxyGitHub(request, response) {
+  console.log('Routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+app.get('/github/*', proxyGitHub);
+
 
 app.get('/', (req, res) => {
   res.sendFile('./index.html', {root: './'})
